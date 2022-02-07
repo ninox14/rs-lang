8222c4a87235178700c6537982107c4a83a36bd4
Wunlei <confusioncosmos@gmail.com>
2022-02-07 23:49:43 +0300

    feat: add footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,22 @@
 import { FC } from 'react';
+import './Footer.scss';
 
-export const Footer: FC = () => <footer>Footer</footer>;
+export const Footer: FC = () => (
+  <footer className="footer">
+    <div className="footer__logo-wrapper">
+      <a href="https://rs.school/js/" className="footer__logo"></a>
+      <span className="footer__year">© 2022</span>
+    </div>
+    <div className="footer__links-wrapper">
+      <a href="https://github.com/ninox14" className="footer__link">
+        ninox14
+      </a>
+      <a href="https://github.com/roninpepe" className="footer__link">
+        roninpepe
+      </a>
+      <a href="https://github.com/wunlei" className="footer__link">
+        wunlei
+      </a>
+    </div>
+  </footer>
+);
