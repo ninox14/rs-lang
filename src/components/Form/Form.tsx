34b189e33e6adf480698e56a44b1b2881e58afc2
NiNoX14 <hardstuckd5lul@gmail.com>
2022-02-09 01:25:00 +0200

    feat: form validation and error inputs styles

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,24 +1,72 @@
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 import './Form.scss';
 
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+// eslint-disable-next-line import/named
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
 interface FomrProps {
-  email?: string;
-  password?: string;
-  type?: 'login' | 'register';
-  // TODO onsubmit
-  onSubmit?: any;
+  type: 'login' | 'register';
+}
+interface FormValues {
+  email: string;
+  password: string;
 }
 
+const validationSchema: yup.SchemaOf<FormValues> = yup.object({
+  email: yup
+    .string()
+    .required('Это поле нужно заполнить')
+    .email('Email вида example@mail.com')
+    .trim('Не должно быть пустых символов в начале'),
+  password: yup
+    .string()
+    .required('Это поле нужно заполнить')
+    .min(8, 'Пароль минимум из 8 символов')
+    .trim('Не должно быть пустых символов в начале'),
+});
+
 export const Form: FC<FomrProps> = ({ type }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: yupResolver(validationSchema),
+    shouldUseNativeValidation: false,
+  });
+
+  // TODO: Onsubmit for api
+  const onSubmit: SubmitHandler<FormValues> = (data) => {};
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit(onSubmit)} noValidate>
       <div className="form__inputs">
-        <TextField type="email" className="input" label="E-mail" variant="outlined" required />
-        <TextField type="password" className="input" label="Password" variant="outlined" required />
+        <TextField
+          type="email"
+          error={!!errors.email}
+          helperText={errors.email ? errors.email.message : null}
+          className="input"
+          label="E-mail"
+          variant="outlined"
+          required
+          inputProps={{ ...register('email') }}
+        />
+        <TextField
+          type="password"
+          error={!!errors.password}
+          helperText={errors.password ? errors.password.message : null}
+          className="input"
+          label="Password"
+          variant="outlined"
+          required
+          inputProps={{ ...register('password') }}
+        />
       </div>
-      <Button className="button" variant="contained">
+      <Button type="submit" className="button" variant="contained">
         {type === 'login' ? 'Войти' : 'Зарегистрироваться'}
       </Button>
     </form>
