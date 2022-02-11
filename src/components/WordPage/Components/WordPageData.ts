bf45f2f321391da26aac41dd812d0b2a54d676c2
Wunlei <confusioncosmos@gmail.com>
2022-02-11 19:28:46 +0300

    feat: add links to groups at words page

---
+++ src/components/WordPage/Components/WordPageData.ts
@@ -0,0 +1,19 @@
+export const groupsColorThemes = [
+  'lightblue',
+  'blue',
+  'pink',
+  'green',
+  'yellow',
+  'orange',
+  'purple',
+];
+
+export const groupsLinksData = [
+  { id: 0, title: 'A1', path: '/textbook/0/0', isAuthReq: false },
+  { id: 1, title: 'A2', path: '/textbook/1/0', isAuthReq: false },
+  { id: 2, title: 'B1', path: '/textbook/2/0', isAuthReq: false },
+  { id: 3, title: 'B2', path: '/textbook/3/0', isAuthReq: false },
+  { id: 4, title: 'C1', path: '/textbook/4/0', isAuthReq: false },
+  { id: 5, title: 'C2', path: '/textbook/5/0', isAuthReq: false },
+  { id: 6, title: '★', path: '/textbook/6/0', isAuthReq: true },
+];
--- src/components/WordPage/WordPage.tsx
+++ src/components/WordPage/WordPage.tsx
@@ -1,9 +1,38 @@
+import SpeedDial from '@mui/material/SpeedDial';
 import { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
+import { groupsColorThemes, groupsLinksData } from './Components/WordPageData';
+import './WordPage.scss';
 
-export const WordPage: FC = () => (
-  <div>
-    <h1>Page</h1>
-    <Outlet />
-  </div>
-);
+export const WordPage: FC = () => {
+  const groupId = Number(useParams().groupId || '0');
+  const isLogged = true;
+
+  return (
+    <div
+      className={`page page_wordpage wordpage_${groupsColorThemes[groupId]}`}
+    >
+      <div className="wordpage-container">
+        <h4 className="wordpage__title">Разделы</h4>
+        <div className="groups-container">
+          {groupsLinksData.map((el) => {
+            if (!el.isAuthReq || isLogged) {
+              return (
+                <Link
+                  key={el.id}
+                  to={el.path}
+                  className={`groups__link groups__link_${
+                    groupsColorThemes[el.id]
+                  } ${groupId === el.id ? 'groups__link_active' : ''}`}
+                >
+                  {el.title}
+                </Link>
+              );
+            }
+          })}
+        </div>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
