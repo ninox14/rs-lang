51651ed25574b533f169410dd4fc4e543c39d496
Pepe the Ronin <89341250+roninpepe@users.noreply.github.com>
2022-02-15 01:35:05 +0300

    refactor: apply some changes according to commits from mui-styling branch

--- src/pages/Team/Team.tsx
+++ src/pages/Team/Team.tsx
@@ -1,45 +1,13 @@
 import AuthorCard from 'components/TeamPage/AuthorCard';
+import authors from 'data/team';
 import { FC, ReactElement } from 'react';
-
-interface Author {
-  name?: string;
-  nickname?: string;
-  github?: string;
-  picture?: string;
-  role?: string;
-}
-
-type Authors = Author[];
-
-const authors: Authors = [
-  {
-    name: 'Nick Kleshchev',
-    nickname: 'ninox14',
-    github: 'https://github.com/ninox14',
-    picture: 'https://avatars.githubusercontent.com/u/31549851',
-    role: '',
-  },
-  {
-    name: 'Mikita Bely',
-    nickname: 'roninpepe',
-    github: 'https://github.com/roninpepe',
-    picture: 'https://avatars.githubusercontent.com/u/89341250',
-    role: '',
-  },
-  {
-    name: 'Polina Sudareva',
-    nickname: 'wunlei',
-    github: 'https://github.com/wunlei',
-    picture: 'https://avatars.githubusercontent.com/u/70818020',
-    role: '',
-  },
-];
+import { Author } from 'types/rs-lang';
 
 export const Team: FC = () => {
   return (
     <main className="page page_team team">
       <div className="team__container">
-        <h1 className="page-header team__header">Над проектом работали</h1>
+        <h1 className="page__header team__header">Над проектом работали</h1>
         <div className="team__cards-container">
           {authors.map((author: Author): ReactElement => (
             <AuthorCard
