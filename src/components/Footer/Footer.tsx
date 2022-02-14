3b751c46b7f7460aef70b1fb6f9a8fb308830ccf
NiNoX14 <hardstuckd5lul@gmail.com>
2022-02-15 00:12:32 +0200

    fix: reverting unintentional changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,33 +4,17 @@ import './Footer.scss';
 export const Footer: FC = () => (
   <footer className="footer">
     <div className="footer__logo-wrapper">
-      <a
-        href="https://rs.school/js/"
-        target="_blank"
-        className="footer__logo"
-      ></a>
+      <a href="https://rs.school/js/" target="_blank" className="footer__logo"></a>
       <span className="footer__year">© 2022</span>
     </div>
     <div className="footer__links-wrapper">
-      <a
-        href="https://github.com/ninox14"
-        target="_blank"
-        className="footer__link"
-      >
+      <a href="https://github.com/ninox14" target="_blank" className="footer__link">
         ninox14
       </a>
-      <a
-        href="https://github.com/roninpepe"
-        target="_blank"
-        className="footer__link"
-      >
+      <a href="https://github.com/roninpepe" target="_blank" className="footer__link">
         roninpepe
       </a>
-      <a
-        href="https://github.com/wunlei"
-        target="_blank"
-        className="footer__link"
-      >
+      <a href="https://github.com/wunlei" target="_blank" className="footer__link">
         wunlei
       </a>
     </div>
