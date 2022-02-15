77d0385718bd815397692702454466415f390d57
Wunlei <confusioncosmos@gmail.com>
2022-02-15 23:15:56 +0300

    feat: implement user login check

--- src/pages/Textbook/Textbook.tsx
+++ src/pages/Textbook/Textbook.tsx
@@ -2,22 +2,39 @@ import { FC } from 'react';
 import { CardTextbook } from './Components/CardTextbook';
 import CardData from './Components/CardData';
 import './Textbook.scss';
+import { useAppSelector } from 'redux/hooks';
 
 export const Textbook: FC = () => {
+  const isLogged = useAppSelector((state) => state.word.userId);
   return (
     <main className="page page_textbook">
       <div className="textbook-content">
         <h2 className="textbook__title">Учебник</h2>
         <div className="textbook__cards-container">
-          {CardData.map((el) => (
-            <CardTextbook
-              key={el.id}
-              title={el.title}
-              desc={el.desc}
-              level={el.level}
-              path={el.path}
-            />
-          ))}
+          {CardData.map((el) => {
+            if (!el.isAuthReq) {
+              return (
+                <CardTextbook
+                  key={el.id}
+                  title={el.title}
+                  desc={el.desc}
+                  level={el.level}
+                  path={el.path}
+                />
+              );
+            }
+            if (isLogged) {
+              return (
+                <CardTextbook
+                  key={el.id}
+                  title={el.title}
+                  desc={el.desc}
+                  level={el.level}
+                  path={el.path}
+                />
+              );
+            }
+          })}
         </div>
       </div>
     </main>
