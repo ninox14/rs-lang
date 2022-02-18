867091435facc6ae5416a470d848800ac846ca19
NiNoX14 <hardstuckd5lul@gmail.com>
2022-02-18 16:33:17 +0200

    feat: pick difficulty and initalize with prefetched words

--- src/components/GameContext/GameContext.tsx
+++ src/components/GameContext/GameContext.tsx
@@ -1,6 +1,17 @@
-import { GameKey } from 'api/ApiService';
-import { createContext, FC } from 'react';
-import { IWord } from 'redux/types/types';
+import type { AnyAction } from '@reduxjs/toolkit';
+import { GameKey, IGetWordsOptions } from 'api/ApiService';
+import { createContext, FC, useEffect, useState } from 'react';
+import {
+  getWordsAudiocall,
+  getWordsAudiocallAnon,
+  getWordsSprint,
+  getWordsSprintAnon,
+} from 'redux/actions';
+import { useAppDispatch, useAppSelector } from 'redux/hooks';
+import { IUserWordsActionOptions, IWord } from 'redux/types/types';
+import { getRandomNumber } from 'utils/helpers';
+
+const maxPages = 30;
 
 export enum GameState {
   INITIAL = 'initial',
@@ -8,6 +19,7 @@ export enum GameState {
   CORRECT = 'answ-correct',
   WRONG = 'answ-wrong',
   RESULTS = 'results',
+  COUNTDOWN = 'countdown',
 }
 
 interface IGameContext {
@@ -30,6 +42,10 @@ const contextDefaults: IGameContext = {
   pickDifficulty: (difficulty) => {},
 };
 
+interface IFetchWordsOptions {
+  action: () => AnyAction;
+  // actionOptions: IUserWordsActionOptions | IGetWordsOptions;
+}
 interface IGameContextProps {
   game: GameKey;
 }
@@ -37,10 +53,103 @@ interface IGameContextProps {
 const GameContext = createContext<IGameContext>(contextDefaults);
 
 export const GameProvider: FC<IGameContextProps> = ({ game, children }) => {
-  return (
-    <GameContext.Provider
-      value={contextDefaults}
-      children={children}
-    ></GameContext.Provider>
+  const appDispatch = useAppDispatch();
+
+  const isRanFromTextBook = useAppSelector(
+    (state) => state.word.isGameRanFromTextbook
   );
+  const sprintWords = useAppSelector((state) => state.word.sprintWords);
+  const audicallWords = useAppSelector((state) => state.word.audiocallWords);
+  const userId = useAppSelector((state) => state.word.userId);
+
+  const [gameState, setGameState] = useState<GameState>(GameState.INITIAL);
+  const [gameWords, setGameWords] = useState<IWord[]>([]);
+
+  const initializeWords = () => {
+    switch (game) {
+      case 'audiocall': {
+        setGameWords(audicallWords);
+        break;
+      }
+      case 'sprint': {
+        setGameWords(sprintWords);
+        break;
+      }
+    }
+  };
+
+  const fetchWords = ({ action }: IFetchWordsOptions) => {
+    switch (game) {
+      case 'audiocall': {
+        appDispatch(action());
+        break;
+      }
+      case 'sprint': {
+        appDispatch(action());
+        break;
+      }
+    }
+  };
+
+  const pickDifficulty = (difficulty: number) => {
+    if (userId) {
+      switch (game) {
+        case 'audiocall': {
+          appDispatch(
+            getWordsAudiocall({
+              userId,
+              group: difficulty,
+              page: getRandomNumber(maxPages),
+            })
+          );
+          break;
+        }
+        case 'sprint': {
+          appDispatch(
+            getWordsSprint({
+              userId,
+              group: difficulty,
+              page: getRandomNumber(maxPages),
+            })
+          );
+          break;
+        }
+      }
+    } else {
+      switch (game) {
+        case 'audiocall': {
+          appDispatch(
+            getWordsAudiocallAnon({
+              group: difficulty,
+              page: getRandomNumber(maxPages),
+            })
+          );
+          break;
+        }
+        case 'sprint': {
+          appDispatch(
+            getWordsSprintAnon({
+              group: difficulty,
+              page: getRandomNumber(maxPages),
+            })
+          );
+          break;
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isRanFromTextBook) {
+      initializeWords();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    initializeWords();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sprintWords, audicallWords]);
+
+  return <GameContext.Provider value={contextDefaults} children={children} />;
 };
