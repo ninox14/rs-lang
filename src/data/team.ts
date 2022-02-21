7ee48a9d03190fa5bb4e199f6cb913d90355d4ba
Wunlei <confusioncosmos@gmail.com>
2022-02-22 01:01:13 +0300

    feat: styles fixes, add team members description

--- src/data/team.ts
+++ src/data/team.ts
@@ -6,21 +6,21 @@ const authors: Authors = [
     nickname: 'ninox14',
     github: 'https://github.com/ninox14',
     picture: 'https://avatars.githubusercontent.com/u/31549851',
-    role: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
+    role: 'Работа с Backend, разработка API игр, страница Авторизации, игра Спринт',
   },
   {
     name: 'Mikita Bely',
     nickname: 'roninpepe',
     github: 'https://github.com/roninpepe',
     picture: 'https://avatars.githubusercontent.com/u/89341250',
-    role: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
+    role: 'Вёрстка, игра Аудиовызов, интеграция API в игру, страница О команде',
   },
   {
     name: 'Polina Sudareva',
     nickname: 'wunlei',
     github: 'https://github.com/wunlei',
     picture: 'https://avatars.githubusercontent.com/u/70818020',
-    role: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
+    role: 'Дизайн, вёрстка основных страниц, страница с карточками, страница статистики',
   },
 ];
 
